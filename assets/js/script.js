7ca55224cfde042dc8bac9acf25f95e0925b5ccb
EMMANUELROJAS01 <392rojas@gmail.com>
2022-08-06 16:47:26 -0700

    changed font size on navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
+        let heads = Math.random() < 0.5
+        if (heads) {
         // Make variables to track the percentages of heads and tails
         // TODO: Use the calculated total to calculate the percentages
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
